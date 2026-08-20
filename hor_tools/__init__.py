9c47149015b79bdd165eba4e79dbb0a9a7c70833
Cybergnostic <Cybergnostic@users.noreply.github.com>
2026-08-20 12:36:27 +0200

    Expose validated parser and lightweight longitude API

--- hor_tools/__init__.py
+++ hor_tools/__init__.py
@@ -1,16 +1,23 @@
-"""Tools for reading Morinus .hor files and computing placements with Swiss Ephemeris."""
+"""Tools for reading Morinus .hor files and computing traditional chart data."""
 
-from .astro_engine import compute_houses, compute_planets, julian_day_from_chart
-from .hor_parser import load_hor
+from .astro_engine import (
+    compute_houses,
+    compute_longitudes,
+    compute_planets,
+    julian_day_from_chart,
+)
+from .hor_parser import HorParseError, load_hor
 from .models import AspectInfo, ChartInput, Houses, PlanetPosition, PlanetReport
 
 __all__ = [
+    "AspectInfo",
     "ChartInput",
+    "HorParseError",
     "Houses",
     "PlanetPosition",
     "PlanetReport",
-    "AspectInfo",
     "compute_houses",
+    "compute_longitudes",
     "compute_planets",
     "julian_day_from_chart",
     "load_hor",
